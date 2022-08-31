48b84d5a35affa09c03ff34cf9374ca2fa0d8394
Visishta-ch <Visishtachatla@gmail.com>
2022-08-31 19:14:37 +0530

    done with error handling

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
 document
   .getElementById('my-form')
   .addEventListener('submit', async function submit(e) {
+    // try{}
     e.preventDefault();
 
     const amount = document.getElementById('amount').value;
     const description = document.getElementById('description').value;
     const category = document.getElementById('category').value;
+    try {
+      const user = {
+        amount,
+        description,
+        category,
+      };
 
-    const user = {
-      amount,
-      description,
-      category,
-    };
+      const response = await axios.post(
+        `https://crudcrud.com/api/66c6b068f02c41c1811c355d369f83/etLog`,
+        user
+      );
 
-    const response = await axios.post(
-      `https://crudcrud.com/api/66c6b068f02c41c1811c355d369f83e9/etLog`,
-      user
-    );
+      if (response.status === 201) {
+        console.log(response.data);
+        displayUserLog(response.data);
 
-    if (response.status === 201) {
-      console.log(response.data);
-      displayUserLog(response.data);
-
-      document.getElementById('my-form').reset();
-      document.getElementById('amount').focus();
-    } else {
-      alert(`Error in saving data`);
+        document.getElementById('my-form').reset();
+        document.getElementById('amount').focus();
+      } else {
+        window.alert(`Error in saving data`);
+      }
+    } catch (error) {
+      alert('Something went wrong.... Try again');
     }
   });
 
 window.onload = async function () {
-  const response = await axios.get(
-    `https://crudcrud.com/api/66c6b068f02c41c1811c355d369f83e9/etLog`
-  );
   try {
+    const response = await axios.get(
+      `https://crudcrud.com/api/66c6b068f02c41c1811c355d369f83e9/etLog`
+    );
+
     for (var i = 0; i < response.data.length; i++) {
       displayUserLog(response.data[i]);
     }
   } catch (err) {
-    console.log(`Oops something went wrong `);
+    alert(`Oops something went wrong `);
   }
 };
 
